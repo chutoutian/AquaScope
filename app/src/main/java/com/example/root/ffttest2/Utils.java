07c5f13a267c1a8fa43af76ec186e2b1b90a52da
beitong95 <beitong2@illinois.edu>
2024-08-24 17:06:34 -0500

    Add Feature: Add SNR plot of proposed method back

--- app/src/main/java/com/example/root/ffttest2/Utils.java
+++ app/src/main/java/com/example/root/ffttest2/Utils.java
@@ -2233,7 +2233,7 @@ public class Utils {
                                 }
 
                                 // plot Spec and SNR
-//                                ChannelEstimate.extractSignal_withsymbol_helper(MainActivity.av, sounding_signal, 0, m_attempt);
+                                ChannelEstimate.extractSignal_withsymbol_helper(MainActivity.av, sounding_signal, 0, m_attempt);
 
                                 Utils.logd("copy ("+xcorr_out[1]+","+filt.length+") to ("+sounding_signal_counter+")");
                             }
@@ -2499,7 +2499,7 @@ public class Utils {
                                 }
 
                                 // plot Spec and SNR
-//                                ChannelEstimate.extractSignal_withsymbol_helper(MainActivity.av, sounding_signal, 0, m_attempt);
+                                ChannelEstimate.extractSignal_withsymbol_helper(MainActivity.av, sounding_signal, 0, m_attempt);
 
                                 Utils.logd("copy ("+xcorr_out[1]+","+filt.length+") to ("+sounding_signal_counter+")");
                             }
